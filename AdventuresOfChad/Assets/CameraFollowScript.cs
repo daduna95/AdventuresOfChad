5f5e0da0138d4b030dec556a07820ad9eace3816
daduna95 <49406977+daduna95@users.noreply.github.com>
2019-12-18 20:58:32 +0430

    Added Scene,CharMove,CameraFollow
    Added Scene,CharMove,CameraFollow

---
+++ AdventuresOfChad/Assets/CameraFollowScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    void Update()
+    {
+        transform.position = target.position;
+    }
+}
---
+++ AdventuresOfChad/Assets/PlayerMoveScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class PlayerMoveScript : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            MovePlayer();
+        }
+    }
+
+    public void MovePlayer()
+    {
+        RaycastHit hit;
+        Ray lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool hasHit = Physics.Raycast(lastRay, out hit);
+        if(hasHit == true)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+}
